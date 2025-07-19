a33ea0e042614dd155acbcab2ec0f5a652304ff4
Boudjemline moad <moad@TechkneWeb.>
2025-07-19 04:39:12 +0200

    pull supabase

---
+++ supabase/migrations/20250719023634_remote_schema.sql
@@ -0,0 +1,109 @@
+create sequence "public"."users_id_seq";
+
+revoke delete on table "public"."products" from "anon";
+
+revoke insert on table "public"."products" from "anon";
+
+revoke references on table "public"."products" from "anon";
+
+revoke select on table "public"."products" from "anon";
+
+revoke trigger on table "public"."products" from "anon";
+
+revoke truncate on table "public"."products" from "anon";
+
+revoke update on table "public"."products" from "anon";
+
+revoke delete on table "public"."products" from "authenticated";
+
+revoke insert on table "public"."products" from "authenticated";
+
+revoke references on table "public"."products" from "authenticated";
+
+revoke select on table "public"."products" from "authenticated";
+
+revoke trigger on table "public"."products" from "authenticated";
+
+revoke truncate on table "public"."products" from "authenticated";
+
+revoke update on table "public"."products" from "authenticated";
+
+revoke delete on table "public"."products" from "service_role";
+
+revoke insert on table "public"."products" from "service_role";
+
+revoke references on table "public"."products" from "service_role";
+
+revoke select on table "public"."products" from "service_role";
+
+revoke trigger on table "public"."products" from "service_role";
+
+revoke truncate on table "public"."products" from "service_role";
+
+revoke update on table "public"."products" from "service_role";
+
+alter table "public"."products" drop constraint "products_pkey";
+
+drop index if exists "public"."products_pkey";
+
+drop table "public"."products";
+
+create table "public"."users" (
+    "id" integer not null default nextval('users_id_seq'::regclass),
+    "email" character varying(255) not null,
+    "name" character varying(255),
+    "username" character varying(255)
+);
+
+
+alter sequence "public"."users_id_seq" owned by "public"."users"."id";
+
+drop sequence if exists "public"."products_id_seq";
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
